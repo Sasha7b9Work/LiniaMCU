89bd7cc964e5c3db038df2eaab8abd8a8b41af62
Sasha7b9Work <Phuntik-it@tut.by>
2025-09-22 13:21:41 +0300

    *

--- sources/Device/src/Hardware/HAL/HAL_PINS.cpp
+++ sources/Device/src/Hardware/HAL/HAL_PINS.cpp
@@ -1,6 +1,7 @@
 // 2025/08/30 18:39:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "defines.h"
 #include "Hardware/HAL/HAL_PINS.h"
+#include <stm32f4xx.h>
 
 
 PinOut pB_3_50_V(Port::_G, Pin::_11);
@@ -49,3 +50,46 @@ PinOut pCLK1_DAC(Port::_D, Pin::_11);
 
 PinOut pDAT2_DAC(Port::_B, Pin::_15);
 PinOut pCLK2_DAC(Port::_D, Pin::_9);
+
+
+Pin::Pin(Port::E _port, Pin::E _pin, PinMode::E _mode)
+{
+    static GPIO_TypeDef *ports[Port::Count] =
+    {
+        GPIOA,
+        GPIOB,
+        GPIOC,
+        GPIOD,
+        GPIOE,
+        GPIOF,
+        GPIOG,
+        GPIOH,
+        GPIOI
+    };
+
+    port = ports[_port];
+
+    static const uint16 pins[Pin::Count] =
+    {
+        GPIO_PIN_0,
+        GPIO_PIN_1,
+        GPIO_PIN_2,
+        GPIO_PIN_3,
+        GPIO_PIN_4,
+        GPIO_PIN_5,
+        GPIO_PIN_6,
+        GPIO_PIN_7,
+        GPIO_PIN_8,
+        GPIO_PIN_9,
+        GPIO_PIN_10,
+        GPIO_PIN_11,
+        GPIO_PIN_12,
+        GPIO_PIN_13,
+        GPIO_PIN_14,
+        GPIO_PIN_15
+    };
+
+    pin = pins[_pin];
+
+    mode = _mode;
+}
--- sources/Device/src/Hardware/HAL/HAL_PINS.h
+++ sources/Device/src/Hardware/HAL/HAL_PINS.h
@@ -13,6 +13,8 @@ struct Port
         _E,
         _F,
         _G,
+        _H,
+        _I,
         Count
     };
 };
@@ -58,7 +60,7 @@ public:
 private:
 
     void      *port;
-    uint16     pint;
+    uint16     pin;
     PinMode::E mode;
 };
 
@@ -76,40 +78,11 @@ public:
     PinOut(Port::E port, Pin::E pin) : Pin(port, pin, PinMode::Out) { }
 };
 
+// Orange Pi
+extern PinOut pPI_ENB_STM;      // PD12 81
+extern Pin    pPI_TX;           // PB6  136
+extern Pin    pPI_RX;           // PA10 102
 
-// Канал базы
-extern PinOut pB_3_50_V;        // PG11 57  Определяет режим высокого напряжения/режим большого тока
-extern PinOut pB_END1B;         // PC3  29  Разрешение DAC
-extern PinOut pB_END2B;         // PC2  28  Разрешение DAC
-extern PinOut pB_ENRGB;         // PC1  27  Разрешение регистра
-
-// Канал подложки
-extern PinOut pS_END1P;         // PC0  26  Разрешение DAC
-extern PinOut pS_END2P;         // PF10 22  Разрешение DAC
-extern PinOut pS_ENDRGP;        // PF9  21  Разрешение регистра
-
-// Измеритель тока
-extern PinOut pMEAS_I_SPEED;    // PG11 126
-extern PinOut pMEAS_I_ENRGI;    //      11  Разрешение регистра
-
-// Формирователь развёртки
-extern PinOut pRAZV_ENDU;       // PC15 9   Разрешение измерителя U блока развёртки
-extern PinOut pRAZV_ENRGF;      // PF0  10  Разрешение регистра
-
-// Источник 50V
-extern PinOut pSOURCE_50V_50EP; // PA6  42
-extern PinOut pSOURCE_50V_50EN; // PF11 49
-
-// Коммутатор
-extern PinIn  pCOMM_3000VE;     // PC4  44
-extern PinIn  pCOMM_200EN;      // PC5  45
-extern PinIn  pCOMM_200EP;      // PB0  46
-extern PinIn  pCOMM_20EN;       // PB1  47
-extern PinIn  pCOMM_20EP;       // PB2  48
-extern PinOut pCOMM_ENRGK;      // PF7  19  Разрешение регистра
-
-// Источник 3 кВ
-extern PinOut pin_SOURCE_3kV_ENRGV; // PF8  20  Разрешение регистра источника напряжения 3кВ
 
 // ПЛИС
 extern PinIn  pFPGA_LIMIT;      // PE7  58
@@ -121,15 +94,38 @@ extern PinOut pFPGA_WR_RG;      // PG8  93
 extern Pin    pFPGA_DAT_RG;     // PD13 82
 extern Pin    pFPGA_ST_TB;      //      15
 
-// Orange Pi
-extern PinOut pPI_ENB_STM;      // PD12 81
-extern Pin    pPI_TX;           // PB6  136
-extern Pin    pPI_RX;           // PA10 102
+// XP10 Источник 50V
+extern PinOut pSOURCE_50V_50EP; // PA6  42
+extern PinOut pSOURCE_50V_50EN; // PF11 49
 
-// Канал базы, канал подложки
+//------------------ Канал базы, канал подложки
 extern PinOut pDAT1_DAC;        // PD8  77
 extern PinOut pCLK1_DAC;        // PD11 80
+// XP4 Канал базы
+extern PinOut pB_3_50_V;        // PG11 57  Определяет режим высокого напряжения/режим большого тока
+extern PinOut pB_END1B;         // PC3  29  Разрешение DAC
+extern PinOut pB_END2B;         // PC2  28  Разрешение DAC
+extern PinOut pB_ENRGB;         // PC1  27  Разрешение регистра
+// XP5 Канал подложки
+extern PinOut pS_END1P;         // PC0  26  Разрешение DAC
+extern PinOut pS_END2P;         // PF10 22  Разрешение DAC
+extern PinOut pS_ENDRGP;        // PF9  21  Разрешение регистра
 
-// Измеритель тока, формирователь развёртки, коммутатор, источник напряжения 3кВ
+//------------------ Измеритель тока, формирователь развёртки, коммутатор, источник напряжения 3кВ
 extern PinOut pDAT2_DAC;        // PB15 76
 extern PinOut pCLK2_DAC;        // PD9  78
+// XP7 Формирователь развёртки
+extern PinOut pRAZV_ENDU;       // PC15 9   Разрешение измерителя U блока развёртки
+extern PinOut pRAZV_ENRGF;      // PF0  10  Разрешение регистра
+// XP13 Источник 3 кВ
+extern PinOut pin_SOURCE_3kV_ENRGV; // PF8  20  Разрешение регистра источника напряжения 3кВ
+// XP12 Коммутатор
+extern PinIn  pCOMM_3000VE;     // PC4  44
+extern PinIn  pCOMM_200EN;      // PC5  45
+extern PinIn  pCOMM_200EP;      // PB0  46
+extern PinIn  pCOMM_20EN;       // PB1  47
+extern PinIn  pCOMM_20EP;       // PB2  48
+extern PinOut pCOMM_ENRGK;      // PF7  19  Разрешение регистра
+// XP6 Измеритель тока
+extern PinOut pMEAS_I_SPEED;    // PG11 126
+extern PinOut pMEAS_I_ENRGI;    //      11  Разрешение регистра
