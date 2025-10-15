6c2dd45980981e72c3a501de44727d47595c47e0
Sasha7b9Work <Phuntik-it@tut.by>
2025-10-15 18:05:07 +0300

    *

--- sources/Device/src/Device/Chips.cpp
+++ sources/Device/src/Device/Chips.cpp
@@ -20,12 +20,12 @@ ChipDAC dacs[10] =
 
 ChipREG regs[10] =
 {
-    { 24, &pin_SOURCE_3kV_ENRGV, &pCLK2_DAC, nullptr },  // Источник напряжения 3000В
-    { 16, &pCOMM_ENRGK,          &pCLK2_DAC, nullptr },  // Коммутатор
-    { 32, &pRAZV_ENRGF,          &pCLK2_DAC, nullptr },  // Формирователь развёртки и измеритель напряжения
-    { 32, &pB_END2B,             &pCLK1_DAC, nullptr },  // Источник-измеритель (канал базы)
-    { 32, &pS_ENDRGP,            &pCLK1_DAC, nullptr },  // Источник-измеритель (дополнительный канал)
-    { 16, &pMEAS_I_ENRGI,        &pCLK2_DAC, nullptr },  // Измеритель тока
+    { 24, &pin_SOURCE_3kV_ENRGV, &pCLK2_DAC, &pDAT2_DAC },  // Источник напряжения 3000В
+    { 16, &pCOMM_ENRGK,          &pCLK2_DAC, &pDAT2_DAC },  // Коммутатор
+    { 32, &pRAZV_ENRGF,          &pCLK2_DAC, &pDAT2_DAC },  // Формирователь развёртки и измеритель напряжения
+    { 32, &pB_END2B,             &pCLK1_DAC, &pDAT1_DAC },  // Источник-измеритель (канал базы)
+    { 32, &pS_ENDRGP,            &pCLK1_DAC, &pDAT1_DAC },  // Источник-измеритель (дополнительный канал)
+    { 16, &pMEAS_I_ENRGI,        &pCLK2_DAC, &pDAT2_DAC },  // Измеритель тока
     { 8,  &pSOURCE_50V_50EN,     nullptr,    nullptr },  // Источник напряжения 50 В
     { 0,  nullptr,               nullptr,    nullptr },
     { 0,  nullptr,               nullptr,    nullptr },
